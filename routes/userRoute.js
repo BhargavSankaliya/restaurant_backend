2f6db8f794908b4e0bc82bc22cf97bba5ea078fc
Meet-Kachhiya <166788093+Meet-Kachhiya@users.noreply.github.com>
2024-12-01 15:38:09 +0530

    UserModel All API Changes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,12 +6,11 @@ const { authController } = require('../controllers/authController');
 const UserModel = require('../models/userModel');
 
 
-//ads create and update api (if update then _id pass in query)
 router.post("/create-user", validateSchema(UserModel), authController.createUser);
-router.post('/updateUserById/:id', authController.updateUserById);
-router.get('/getUsersByStatus', authController.getUsersByStatus);
-router.post('/toggleUserStatus/:id', authController.toggleUserStatus);
-router.get('/getUserById/:id', authController.getUserById);
+router.post('/updateUserById', authController.updateUserById);
+router.post('/getUsersList', authController.getUsersList);
+router.post('/toggleUserStatus', authController.toggleUserStatus);
+router.post('/getUserById', authController.getUserById);
 
 
 module.exports = router
