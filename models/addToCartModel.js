9ffd93ef04b0ac6f3c7d66553d10a8df22d82d6e
Meet-Kachhiya <166788093+Meet-Kachhiya@users.noreply.github.com>
2024-12-26 23:27:42 +0530

    Change Add To CartSchema

--- models/addToCartModel.js
+++ models/addToCartModel.js
@@ -2,6 +2,52 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const commonSchema = require("./CommonModel");
 
+const itemSchema = new mongoose.Schema(
+  {
+    itemId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, "Item ID is required."],
+      ref: "Items",
+    },
+    quantity: {
+      type: Number,
+      required: [true, "Quantity is required."],
+      min: [1, "Quantity must be at least 1."],
+      default: 1,
+    },
+    option: {
+      name: {
+        type: String,
+        required: true
+      },
+      price: {
+        type: String,
+        required: true
+      }
+    },
+    choices: [
+      {
+        name: {
+          type: String,
+          required: false
+        },
+        itemId: [
+          {
+            type: mongoose.Schema.Types.ObjectId,
+            required: false
+          }
+        ]
+      }
+    ],
+    modifiers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        required: false,
+      },
+    ]
+  },
+  { timestamps: true }
+);
 
 const addToCartSchema = new mongoose.Schema(
   {
@@ -15,21 +61,7 @@ const addToCartSchema = new mongoose.Schema(
       required: [true, "Restaurant ID is required."],
       ref: "Restaurant",
     },
-    items: [
-      {
-        itemId: {
-          type: mongoose.Schema.Types.ObjectId,
-          required: [true, "Item ID is required."],
-          ref: "Items",
-        },
-        quantity: {
-          type: Number,
-          required: [true, "Quantity is required."],
-          min: [1, "Quantity must be at least 1."],
-          default: 1,
-        },
-      },
-    ],
+    items: [itemSchema],
   },
   { timestamps: true }
 );
